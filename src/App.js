9433a38672e0d75e0a8ee2527f1b3b0b2493de63
subhan300 <64373402+subhan300@users.noreply.github.com>
2020-12-13 21:19:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>learning netlify functions</h1>
-
+     <h2>link: https://trusting-galileo-1953df.netlify.app/</h2>
     
       </header>
     </div>
